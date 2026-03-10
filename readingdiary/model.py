5dde6fb393e6a717692522adcadd14a164a19ea8
Sebastian Velasquez <sebastian103318@gmail.com>
2026-03-09 19:56:27 -0500

    implementacion de metodos de clase como add note, set rating, y get notes of pages, ademas de el metodo str de la clase book

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -22,3 +22,38 @@ class Book:
         self.pages: int = pages
         self.rating: int = Book.UNRATED
         self.notes: list[Note] = []
+
+    def add_note(self, text: str, page: int, date: datetime) -> bool:
+        if page > self.pages:
+            return False
+
+        note = Note(text, page, date)
+        self.notes.append(note)
+        return True
+
+    def set_rating(self, rating: int) -> bool:
+        if rating not in (Book.EXCELLENT, Book.GOOD, Book.BAD):
+            return False
+        self.rating = rating
+        return True
+
+    def get_notes_of_page(self, page: int) -> list[Note]:
+        notas = []
+
+        for note in self.notes:
+            if note.page == page:
+                notas.append(note)
+
+        return notas
+
+    def page_with_most_notes(self) -> int:
+        pass
+
+
+    def __str__(self):
+        return f"ISBN: {self.isbn}\nTitle: {self.title}\nAuthor: {self.author}\nPages: {self.pages}\nRating: {self.rating}"
+
+
+
+
+
