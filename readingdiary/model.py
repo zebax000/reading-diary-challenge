1fad598cd1b209433dcc996ddddd53609bf27524
Sebastian Velasquez <sebastian103318@gmail.com>
2026-03-09 19:06:14 -0500

    implementacion clase note, su metodo str, y parte de clase book

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,24 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Note:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str = text
+        self.page: int = page
+        self.date: datetime = date
+
+    def __str__(self):
+        return f"{self.date} - page {self.page}: {self.text}"
+
+class Book:
+    EXCELLENT: int = 3
+    GOOD: int = 2
+    BAD: int = 1
+    UNRATED: int = -1
+
+    def __init__(self, isbn: str, title: str, author: str, pages: int):
+        self.isbn: str = isbn
+        self.title: str = title
+        self.author: str = author
+        self.pages: int = pages
+        self.rating: int = Book.UNRATED
+        self.notes: list[Note] = []
